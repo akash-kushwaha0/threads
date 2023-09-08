b449e2093e1f6e12b7b789f44aa065fdb15dac02
Akash Kushwaha <akashkushwaha76986.com>
2023-09-08 11:05:27 +0530

    MongoDb setup

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
     user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ( { user, btnTitle }: Props) => {
   const [files, setfiles] = useState<File[]>([])
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -81,7 +85,21 @@ const AccountProfile = ( { user, btnTitle }: Props) => {
             values.profile_photo = imgRes[0].fileUrl;
           }
         }
-        
+
+        await updateUser({
+          userId: user.id,
+          username: values.username,
+          name: values.name,
+          bio:  values.bio,
+          image: values.profile_photo,
+          path: pathname
+        });
+
+        if(pathname === '/profile/edit'){
+          router.back();
+        } else {
+          router.push('/');
+        }
       }
 
     return (
