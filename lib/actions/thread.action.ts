5956680d27aeb646554b3441d95c9cd22a4adf78
Akash Kushwaha <akashkushwaha76986.com>
2023-09-14 11:54:02 +0530

    Added Comment

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -66,3 +66,78 @@ export async function fetchPosts(pageNumber = 1, pageSize = 2){
 
     return { posts, isNext };
 }
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+
+    try {
+
+        // TODO: Populate Community
+        const thread = await Thread.findById(id)
+        .populate({
+            path: 'author',
+            model: User,
+            select: "_id id name image"
+        })
+        .populate({
+            path: 'children',
+            populate: [
+                {
+                    path: 'author',
+                    model: User,
+                    select: "_id id name parentId image"
+                },
+                {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    }
+                }
+            ]
+        }).exec();
+
+        return thread;
+    } catch (error: any) {
+        throw new Error(`Error fetching thread: ${error.message}`)
+    }
+}
+
+export async function addCommentToThread(
+    threadId: string,
+    commentText: string,
+    userId: string,
+    path: string,
+) {
+    connectToDB();
+    try {
+        // Find the original thread by its ID
+        const originalThread = await Thread.findById(threadId);
+
+        if(!originalThread) {
+            throw new Error("Thread not found")
+        }
+
+        // Create a new tread with the comment text
+        const commentThread = new Thread({
+            text: commentText,
+            author: userId,
+            parentId: threadId,
+        })
+
+        // Save the new thread
+        const saveCommentThread = await commentThread.save();
+
+        // Update the original thread to incluce the new comment
+        originalThread.children.push(saveCommentThread._id);
+
+        // Save the original thread
+        await originalThread.save();
+
+        revalidatePath(path);
+    } catch (error: any) {
+        throw new Error(`Error adding comment to thread: ${error.message}`)
+    }
+}
